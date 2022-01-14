5894b894a6c96dc6125f3bdb2856fdc207e29116
Alexander Fogal <92050510+afogal1@users.noreply.github.com>
2022-01-14 13:34:42 -0600

    update

--- rock_paper_scissors_project/rock.py
+++ rock_paper_scissors_project/rock.py
@@ -4,27 +4,43 @@ game_active = True
 
 while game_active:
     print("Lets play Rock, Paper, Scissors")
+    win = 0
+    lose = 0
     name = input("What's your name? ")
     print(f"Hey " +  name.title() + " input your command!")
     playing_active = True
     while playing_active:
-        option = input("Rules, play, change name, or quit. : ")
+        option = input("rules, play, or quit. : ")
         if option.lower() == "rules":
-            print("blah blah blah")
+            print("The rock is a closed fist; paper is a flat hand with fingers and thumb extended and the palm facing downward; and scissors is a fist with the index and middle fingers fully extended toward the opposing player. Rock wins against scissors; paper wins against rock; and scissors wins against paper.")
+        elif option.lower() == "quit":
+                print(f"Thanks for playing {name}!")
+                playing_active = False
         elif option.lower() == "play":
-            user_answer = input("choose your weapon. Rock/Paper/Scissors :  ")
-            if user_answer == computer:
+            user_answer = input("choose your weapon. rock/paper/scissors :  ")
+            comp_int = randint(1,3)
+            if comp_int == 1:
+                computer_answer = "rock"
+            elif comp_int == 2:
+                computer_answer = "paper"
+            else:
+                    computer_answer = "scissors"
+            if user_answer.lower() == computer_answer:
                 print("Tie!")
-            elif (user_answer == "rock" and computer == "scissors") or (user_answer == "paper" and computer == "rock") or (user_answer == "scissors" and computer == "paper"):
-                print("You win!")
-            elif (user_answer == "scissors" and computer == "rock") or (user_answer == "rock" and computer == "paper") or (user_answer == "paper" and computer == "scissors"):
-                print("You lose!")
-            
-            
-            else: 
-                print("...")
+            elif (user_answer.lower() == "rock" and computer_answer == "scissors") or (user_answer.lower() == "paper" and computer_answer == "rock") or (user_answer.lower() == "scissors" and computer_answer == "paper"):
+                win += 1
+                print(f"You win, Congratulation {name}!")
+            elif (user_answer.lower() == "scissors" and computer_answer == "rock") or (user_answer.lower() == "rock" and computer_answer == "paper") or (user_answer.lower() == "paper" and computer_answer == "scissors"):
+                lose += 1
+                print("You lose, better luck next time {name}!")
+            print(f"Wins: {win} loses: {lose}")
+        else: 
+            print("That's not a valid play. Check your spelling!")
 
-            
+        
+
+    game_active = False
+
 
     
     
